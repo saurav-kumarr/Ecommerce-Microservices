f2df26982b68be7ce8dc0fd10aad60117a8b0817
Saurav Kumar <csaurav2014@gmail.com>
2025-09-13 11:07:13 +0530

    Application initial structure and files

---
+++ src/main/java/com/ecommerce/app/EcommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+    }
+
+}
